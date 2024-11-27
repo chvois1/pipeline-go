1f2667b6a6759f6b6254070a889a3888b02d7341
Christophe VOISIN <christophe.voisin.e@thalesdigital.io>
2024-11-27 12:30:10 +0100

    init

---
+++ 00-oop/main.go
@@ -0,0 +1,60 @@
+// package main is a sample
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Operator interface {
+	Init(...int)
+	Multiply(int)
+	Add(int)
+	Display()
+}
+
+type Calc struct {
+	mu     sync.Mutex
+	values []int
+}
+
+func (c *Calc) Init(values ...int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.values = values
+}
+
+func (c *Calc) Multiply(k int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for i := 0; i < len(c.values); i++ {
+		c.values[i] = k * c.values[i]
+	}
+}
+func (c *Calc) Add(k int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for i := 0; i < len(c.values); i++ {
+		c.values[i] = k + c.values[i]
+	}
+
+}
+
+func (c *Calc) Display() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for _, v := range c.values {
+		fmt.Println(v)
+	}
+}
+
+var _ Operator = (*Calc)(nil)
+
+func main() {
+	o := &Calc{}
+	o.Init(1, 2, 3, 4)
+	o.Multiply(2)
+	o.Add(1)
+	o.Multiply(2)
+	o.Display()
+}
