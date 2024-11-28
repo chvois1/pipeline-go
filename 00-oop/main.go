4a5cd0c2f451db7617fc76e9c0338040e7fa3007
Christophe VOISIN <christophe.voisin.e@thalesdigital.io>
2024-11-28 10:51:10 +0100

    init

--- 00-oop/main.go
+++ 00-oop/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 )
 
@@ -47,13 +48,66 @@ func (c *Calc) Display() {
 	}
 }
 
-var _ Operator = (*Calc)(nil)
+type operatorSet map[string]struct{}
+
+var (
+	_         Operator = (*Calc)(nil)
+	operators          = operatorSet{
+		"eq":  struct{}{},
+		"in":  struct{}{},
+		"con": struct{}{},
+		"gt":  struct{}{},
+		"lt":  struct{}{},
+	}
+)
+
+// aad adds an op to the set
+func (s operatorSet) add(op string) {
+	s[op] = struct{}{}
+}
+
+// remove removes an op from the set
+func (s operatorSet) remove(op string) {
+	delete(s, op)
+}
+
+// has returns a boolean value describing if the op exists in the set
+func (s operatorSet) has(op string) bool {
+	op = strings.TrimSpace(strings.ToLower(op))
+	_, ok := s[op]
+	return ok
+}
 
 func main() {
+	var wg sync.WaitGroup
+	fmt.Println("sequential processing")
 	o := &Calc{}
 	o.Init(1, 2, 3, 4)
 	o.Multiply(2)
 	o.Add(1)
 	o.Multiply(2)
 	o.Display()
+
+	o = &Calc{}
+	o.Init(1, 2, 3, 4)
+	wg.Add(3)
+	fmt.Println("concurrent processing")
+	go func() {
+		defer wg.Done()
+		o.Multiply(2)
+	}()
+	go func() {
+		defer wg.Done()
+		o.Add(1)
+	}()
+	go func() {
+		defer wg.Done()
+		o.Multiply(2)
+	}()
+	wg.Wait()
+	o.Display()
+
+	if operators.has("EQ") {
+		fmt.Printf("found operator [%s] into the set of operators\n", "EQ")
+	}
 }
